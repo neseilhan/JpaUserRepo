df66da3da0ec987cc6706aa2c30527e00290cdf4
Nese <neseilhannn@gmail.com>
2025-02-23 15:54:52 +0300

    .

--- src/main/java/com/jparepo/demo/entity/User.java
+++ src/main/java/com/jparepo/demo/entity/User.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 @Entity
 @Data
 @Builder
-@Table(name = "users")
+//@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(generator = "UUID", strategy = GenerationType.AUTO)
